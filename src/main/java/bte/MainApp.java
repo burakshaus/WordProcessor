f5cd436f9ae23296593e75d6728b12b9bab1ef0b
Ahmet Burak Can <118999270+burakshaus@users.noreply.github.com>
2026-01-05 23:48:33 +0300

    Wire Open Manu to load HTML into editor

--- src/main/java/bte/MainApp.java
+++ src/main/java/bte/MainApp.java
@@ -4,19 +4,27 @@ import javafx.application.Application;
 import javafx.scene.Scene;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.web.HTMLEditor;
+import javafx.stage.FileChooser;
 import javafx.stage.Stage;
 import javafx.scene.control.Menu;
 import javafx.scene.control.MenuBar;
 import javafx.scene.control.MenuItem;
 
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+
 
 public class MainApp extends Application {
+
+    private File currentFile;
+    private HTMLEditor editor;
 
     @Override
     public void start(Stage stage) {
         // 1. Initialize the HTMLEditor
         // This component includes Bold, Italic, Underline, and Font tools by default
-        HTMLEditor editor = new HTMLEditor();
+        editor = new HTMLEditor();
         editor.setPrefHeight(600);
 
         // 2. Use a BorderPane to let the editor fill the window
@@ -27,7 +35,7 @@ public class MainApp extends Application {
         MenuItem newItem = new MenuItem("New");
         MenuItem openItem = new MenuItem("Open");
         MenuItem saveAsItem = new MenuItem("Save as");
-
+        openItem.setOnAction( e -> openFile(stage));
         fileMenu.getItems().addAll(newItem, openItem, saveAsItem);
         MenuBar menuBar = new MenuBar(fileMenu);
         root.setTop(menuBar);
@@ -40,6 +48,25 @@ public class MainApp extends Application {
         stage.setTitle("Burak's Word Application");
         stage.setScene(scene);
         stage.show();
+    }
+
+    private void openFile(Stage stage) {
+        FileChooser chooser = new FileChooser();
+        chooser.getExtensionFilters().add(
+                new FileChooser.ExtensionFilter("Html Files", "*.html")
+        );
+
+        File file = chooser.showOpenDialog(stage);
+        if (file != null) {
+            try {
+                String html = Files.readString(file.toPath());
+                editor.setHtmlText(html);
+                currentFile = file ;
+            }
+            catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
     }
 
     public static void main(String[] args) {
